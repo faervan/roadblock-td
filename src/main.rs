0c30281c41939cbd0870968db78afb73a99cff8b
DavidDieLandGurke <943kingpl@gmail.com>
2025-03-30 06:24:35 +0200

    remove log shenanigans

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use bevy::{
-    log::{Level, LogPlugin},
-    prelude::*,
-};
+use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use enemy::EnemyPlugin;
 use fastrand::Rng;
@@ -20,7 +17,7 @@ mod ui;
 fn main() {
     let mut app = App::new();
 
-    let mut default_plugins = DefaultPlugins.set(WindowPlugin {
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
             #[cfg(debug_assertions)]
             present_mode: bevy::window::PresentMode::AutoNoVsync,
@@ -28,16 +25,7 @@ fn main() {
             ..default()
         }),
         ..default()
-    });
-
-    // only show debug logs on a debug build, unless the user specifies to keep the log level sane
-    #[cfg(debug_assertions)]
-    if !std::env::args().any(|a| a == "--ks" || a == "--keep-sanity") {
-        default_plugins = default_plugins.set(LogPlugin {
-            level: Level::DEBUG,
-            ..default()
-        });
-    }
+    }));
 
     if std::env::args().any(|a| a == "--egui") {
         app.add_plugins(WorldInspectorPlugin::new());
@@ -48,7 +36,6 @@ fn main() {
     app.insert_resource(RngResource(Rng::new()));
 
     app.add_plugins((
-        default_plugins,
         animation::AnimationPlugin,
         GridPlugin,
         MapPlugin,
